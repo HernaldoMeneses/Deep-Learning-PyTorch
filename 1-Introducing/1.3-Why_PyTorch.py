84255a20e1d82cf2c219d9a27ae8e81453451ba1
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-03-05 10:15:22 -0300

    1.3-Why_PyTorch.py

---
+++ 1-Introducing/1.3-Why_PyTorch.py
@@ -0,0 +1,11 @@
+'''
+    Simplicity
+        easy to learn, use, extende and debug.
+'''
+
+'''
+    1.3.1 - Deep learning revolution
+'''
+
+#pag. 11/141
+#file:///L:/Linear_/Base-TI/Deep-Learning-with-PyTorch.pdf

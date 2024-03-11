0601c91a8ea4e3eb966369a7856c56a72e46d80f
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-03-11 14:52:49 -0300

    Created 1.3.1-deep-learning-revolution.py

---
+++ 1-Introducing/1.3.1-deep-learning-revolution.py
@@ -0,0 +1,11 @@
+#Where PyTorch fits into the current and historical landscape of deep learning tools.
+# Until the late 2000s
+##  "machine learning" relied heavily on feature engineerig.
+### Features are tranformations of input data resulting in numerical features that facilitate a downstream algorithm
+### , such as a classifier, to produce correct outcomes on new data.
+
+# Deep learning
+## on the other hand, deals with finding such representations automatically, from raw data, to perform a task successfully.
+
+#12/141
+#1.3.2 - immediate versus deferred execution
